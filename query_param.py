7643fe9c9f748013f9e9466816d7be0ea3aa585e
yeonwoo <rlckrl0315db@gmail.com>
2023-02-24 18:01:06 +0900

    first commit

---
+++ query_param.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from typing import Union
+
+app = FastAPI()
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+@app.get("/items1/{item_id}")
+async def read_item1(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+@app.get("/items2/{item_id}")
+async def read_item2(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/items3/{item_id}")
+async def read_user_item1(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+@app.get("/items4/{item_id}")
+async def read_user_item2(
+    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
